b12bdca7801b324dba00f19d08d4bd0f6d3a4dfa
krajewskipatryk <krajewskipatryk@onet.pl>
2024-03-07 19:20:59 +0100

    Init commit

---
+++ src/main/java/com/playground/pgapp/PgappApplication.java
@@ -0,0 +1,13 @@
+package com.playground.pgapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playground/pgapp/PgappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playground.pgapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
